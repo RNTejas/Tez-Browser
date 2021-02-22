34a824c9292e19640f1d48bc93b5e355d63d4bbf
Tejas Naik <64312402+RNTejas@users.noreply.github.com>
2021-02-22 21:11:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import *   # pip install PyQt5
 from PyQt5.QtCore import *
-from PyQt5.QtWebEngineWidgets import *
+from PyQt5.QtWebEngineWidgets import *  # pip install PyQtWebEngine
 import sys
 
 
